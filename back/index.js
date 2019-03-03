72306a6785c26572fbd1edbfa9061e3af98dc7d1
Alexis Daniel Zazueta Ortega <alexisdaniel.zazueta.next@bbva.com>
2019-03-03 00:25:30 -0600

    Added basic login functions

--- back/index.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const app = require('./app');
-const config = require('./config');
-
-app.listen(config.port, () => {
-  console.log(`API REST corriendo en el puerto ${config.port}`);
-});
