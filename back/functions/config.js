1b8404cafe9bd82ec16b0802b8ce7d60215ef0bf
Alexis Daniel Zazueta Ortega <alexisdaniel.zazueta.next@bbva.com>
2019-03-03 00:35:49 -0600

    Setting config credentials file and ignored

---
+++ back/functions/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  admin: {
+    type: "service_account",
+    project_id: "redde-df785",
+    private_key_id: "193375b82b8989c971ba612d03200107f4c83350",
+    private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDM1Xj+pAB3JOIO\nVTfcVVSpYTAuztxdZnAu0aURTtt2vsuf1aU4FC94D9iOBk0PQVFeARf5TnVCtWHo\n9rZcPxcV7qYfKdriAQoKm6BDt9IgdLjGPTP/NjIbyVqWpotp5No9DhP2halHEdg/\nkHYgy0Rs+ShoER1HOtYS19ryzwwohHhyQ3YNBW2wpo+KiWxRUw0OJhdVwxHIi5JV\nba7SDyNHqaCinAIYdPKuE6jJF0EdkFC/4/x/QG6a3obMH9lGLnrICzRkanzt4gom\nfka9Qh7ZDMhvr1WNkOYlbLrQVT4XZ0rLZ3Z+m5wHs1m5Th4H/NITJUGDrUOq2YMT\n2ry8CgxTAgMBAAECggEAPtGdSOL9LY0HMwQZnJ4i18txnnlchdx7vTAYYqV/kvxS\nsJMyF2/BWx3o+S2fhnUuwhitxIy2U54TCAJZzRKgPt6w3c/VTAirjrNJxFrku4Vw\ngYOVpch2N6VZ/efXy8Me1yixIKK/Vod3fY8NW2Nbedwz8y6EfyrhKmg+wsaS4P2q\n7FdAFB/2gtil5QR4jyx74KmSV5zNQsNoXjY9JrUNmEkDWc172gP+BV+07Pg8Gb6J\nVxvA7OmZosodZ16dQl+a2zm62Lk5irmWN+O8+tqpdQtWL/78HUXe51/WU5atMimi\noVlob31UI1IzFdcxD3MxHzMFZb1W2eSWa15n+NtDcQKBgQDubB8UxpOdn+Poob9P\nYWKkBlg/gLHqBTnUhBQNyfhU0lFxxu0a/8kNtvJf5yXZmnH9hlZFRAXIwyyLu4KN\n3+vRZizALeMBzOlvj283D5Avt03TdtrDKsG9ry9ewDcbin4OxK2KRayeA2W3n1G3\nA0orHKNhthtSbuZ9P1ntuqowewKBgQDb72rMGF/e+51wG0Lqag95nLTAUUnmakrP\nlzX/yFAEW1p0cUShR137Y/U/ziXQ5eKqBNjMtcpDQOsaflfRtfUNbud1Q6+ChFFU\nh+B/IcwD8p8wIYKOo1W1frnNrxMokHeCRVCuF8UA1g0TwLjLwmyd/bozwVvUBn/d\nL+FZsdeICQKBgQDaq/flyithimAx4Qfh+mPKJLeTpqmHBbwAmhL6gp9szGk+fUDC\nXo88MOsj4pDof6rE6A0AAl97vuaAUj5veUyEBb5NlMh7wkDnvRo32eJYoTqUFJKY\nJgMDwkUkFg91BxSO3vx7sWOcmWkVfN4piK7h06ZV2/w7ItPrU8tbKrUlOQKBgELl\nediGeXK7mdGlmR7wD7Dv51Kv5wh8wDCQT85zQVNx7fuJEFT1/jIe31P/AiWeJumL\n+92YVeNK9bfjQhb5f4YMe76BTfsnAA30HmbNAt8Yhy7+olV8fO9tklprrCokdxVa\nvAWYGUX91ZByiEMB6kPMDzz6zVGagH3ouGSt4V7xAoGBAKnXmZsqf6h92SfVGACy\nUeaQW3Cxt9uSRTMIEpTwyvztfyRDbbT7b16gI1sptHf/YY8xEv+0/9/y9GpMKHMH\nuSWYu7CaxJVIAaTpwRsXGb6PZTNG1uOYXHStzxjy9DhQX9FJPxXq1dhYdSNk93E9\nYSF+pHYqtpfarfQQMXVJKlmp\n-----END PRIVATE KEY-----\n",
+    client_email: "firebase-adminsdk-eiklu@redde-df785.iam.gserviceaccount.com",
+    client_id: "100247522992941420186",
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-eiklu%40redde-df785.iam.gserviceaccount.com"
+  },
+  databaseURL: "https://redde-df785.firebaseio.com",
+  firebase: {
+    apiKey: "AIzaSyDac6ocoFs91dFtNZRqUZY8_wWHKY91b5I",
+    authDomain: "redde-df785.firebaseapp.com",
+    databaseURL: "https://redde-df785.firebaseio.com",
+    projectId: "redde-df785",
+    storageBucket: "redde-df785.appspot.com",
+    messagingSenderId: "1078463867450"
+  }
+}
